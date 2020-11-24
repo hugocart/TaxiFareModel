d2c94853318c3b90887d2e7372e6e8a88b0324ad
Hugo Cart <carthugo@gmail.com>
2020-11-24 16:37:24 +0100

    exo 3

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,4 +1,14 @@
 # imports
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import RobustScaler, OneHotEncoder
+from sklearn.base import BaseEstimator,  TransformerMixin
+from sklearn.model_selection import train_test_split
+from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
+from TaxiFareModel.data import get_data,clean_data
+from TaxiFareModel.utils import compute_rmse
 
 class Trainer():
     def __init__(self, X, y):
@@ -12,22 +22,35 @@ class Trainer():
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
-        pass
+        pipe_distance = make_pipeline(DistanceTransformer(), RobustScaler())
+        pipe_time = make_pipeline(TimeFeaturesEncoder(time_column='pickup_datetime'), OneHotEncoder(handle_unknown='ignore'))
+        dist_cols = ['pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude']
+        time_cols = ['pickup_datetime']
+        feat_eng_bloc = ColumnTransformer([('time', pipe_time, time_cols),
+                                      ('distance', pipe_distance, dist_cols)]
+                                      )
+        self.pipeline = Pipeline(steps=[('feat_eng_bloc', feat_eng_bloc),
+                                ('regressor', RandomForestRegressor())])
+        return self.pipeline
+
 
     def run(self):
         """set and train the pipeline"""
-        pass
+        self.pipeline = self.set_pipeline()
+        self.pipeline.fit(self.X,self.y)
+        return self
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
-        pass
-
+        y_pred = self.pipeline.predict(X_test)
+        return compute_rmse(y_pred, y_test)
 
 if __name__ == "__main__":
-    # get data
-    # clean data
-    # set X and y
-    # hold out
-    # train
-    # evaluate
-    print('TODO')
+    df = get_data()
+    df = clean_data(df)
+    X = df.drop(columns=['fare_amount'])
+    y = df.fare_amount
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+    trainer = Trainer(X=X_train,y=y_train)
+    trainer.run()
+    print("RMSE:",trainer.evaluate(X_test = X_test, y_test = y_test))
